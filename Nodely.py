be8fadae3dad31273af6fc27e965c9e3ef3d2c5c
Toddles <tj@example.nope>
2022-04-13 12:46:19 -0500

    Final version of code, ready for user feedback

--- Nodely.py
+++ Nodely.py
@@ -1,34 +1,16 @@
 #!/usr/bin/env python3
 
-class Node:
+def gSorter(game):
+	return game.data['score']
+
+class Game:
 	def __init__(self, game):
 		self.name = game
-		self.head = None
-		self.tail = None
 		self.data = {}
 		
 	def __repr__(self):
 		return self.name
-	
-	def addTail(self,node):
-		if self.tail:
-			node.tail = self.tail
-			self.tail.head = node
-			node.head = self
-			self.tail = node
-		else:
-			node.head = self
-			self.tail = node
-		
-	def addHead(self,node):
-		if self.head:
-			self.head.tail = node
-			node.head = self.head
-			self.head = node
-			node.tail = self
-		else:
-			self.head = node
-			node.tail = self
+
 			
 class GameList:
 	def __init__(self,game):
@@ -36,8 +18,34 @@ class GameList:
 		self.high_score = game.data['score']
 		self.bottom_game = game
 		self.low_score = game.data['score']
+		self.list = [game]
+
+	#add a node to the GameList
+	def addGame(self,game):
+		self.list.append(game)
+		self.list.sort(key=gSorter,reverse=True)
+		self.top_game = self.list[0]
+		self.high_score = self.list[0].data['score']
+		self.bottom_game = self.list[-1]
+		self.low_score = self.list[-1].data['score']
+
 		
-				
-		
-	
-				
+			
+#tests:
+		#test1 = Game('The Testing')
+		#test1.data['score'] = 24
+		#test2 = Game('Call of the Test')
+		#test2.data['score'] = 21
+		#test3 = Game('The Testing IV')
+		#test3.data['score'] = 5
+		#test4 = Game('Test Not')
+		#test4.data['score'] = 48
+		#test5 = Game('Who Done Tested it Now')
+		#test5.data['score'] = 16
+
+		#testList = GameList(test1)
+		#print(testList.list)
+		#testList.addGame(test2)
+		#print(testList.list)
+		#testList.addGame(test4)
+		#print(testList.list)
--- VG_Recommendation.py
+++ VG_Recommendation.py
@@ -2,9 +2,12 @@
 #Program to recommend video game
 
 import csv
-from Nodely import Node
+from Nodely import Game
+from Nodely import GameList
 
 #setting up game library class
+GameDictionary = {}
+
 
 #setting up background tree to help with searches
 with open('metascrape.csv', newline='') as videogames:
@@ -16,10 +19,78 @@ with open('metascrape.csv', newline='') as videogames:
 		if index < 2:
 			index += 1
 			continue
-		newNode = Node(row[0])
+		newNode = Game(row[0])
 		newNode.data['system'] = row[1]
 		newNode.data['genre'] = row[7]
 		newNode.data['score'] = row[9]
 		newNode.data['description'] = row[6]
 		#add new node to correct location in storage
-		
+		#check to see if the system is already in place
+		if newNode.data['system'] in GameDictionary:
+			#check to see if the genre is already in place
+			if newNode.data['genre'] in GameDictionary[newNode.data['system']]:
+				#if the genre exists, a gamelist exists, so add the new game to the game list
+				GameDictionary[newNode.data['system']][newNode.data['genre']].addGame(newNode)
+			else:
+				GameDictionary[newNode.data['system']][newNode.data['genre']] = GameList(newNode)
+		else:
+			GameDictionary[newNode.data['system']] = {}
+			GameDictionary[newNode.data['system']][newNode.data['genre']] = GameList(newNode)
+				
+#begin the requesting part of the program
+
+print("Hello! Welcome to the Game Library Assistant")
+print("The systems we current support are:")
+systemList = list(GameDictionary.keys())
+keepRolling = True
+while keepRolling:
+	print(', '.join(systemList))
+
+	userSys = ''
+	while userSys not in systemList:
+		userSys = input("Which system from above are you using?: ")
+	print("Excellent!")
+
+	genreList = list(GameDictionary[userSys].keys())
+	print("These are the genre's we have for that system:")
+	print(', '.join(genreList))
+	userGenr = ''
+	while userGenr not in genreList:
+		userGenr = input("Which genre are you interested in?: ")
+	
+	print("Excellent!")
+	print("Finally, would you like the top 10, bottom 10 or all games within that category?")
+	userSelect = ''
+	userOpt = ['TOP 10','BOTTOM 10','ALL']
+	while userSelect not in userOpt:
+		userSelect = input("Please type 'TOP 10', 'BOTTOM 10', or 'ALL': ").upper()
+
+	#return the user's selection
+	print()
+	if userSelect == 'TOP 10':
+		for i in range(10):
+			focalGame = GameDictionary[userSys][userGenr].list[i]
+			print(focalGame)
+			print(focalGame.data['description'])
+			print(focalGame.data['score'])
+			print()
+	elif userSelect == 'BOTTOM 10':
+		for i in range(10):
+			focalGame = GameDictionary[userSys][userGenr].list[-i]
+			print(focalGame)
+			print(focalGame.data['description'])
+			print(focalGame.data['score'])
+			print()
+	else:
+		for i in range(len(GameDictionary[userSys][userGenr])):
+			focalGame = GameDictionary[userSys][userGenr].list[i]
+			print(focalGame)
+			print(focalGame.data['description'])
+			print(focalGame.data['score'])
+			print()
+			
+	#see if the user wants to look up other games
+	continueQ = input("Would you like to search another group of games? Y or N: ").upper()
+	if continueQ == 'N':
+		keepRolling = False
+
